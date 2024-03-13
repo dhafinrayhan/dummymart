c5e9b7e5ed614bfd3db638534c66bdb0ae9606f4
Dhafin Rayhan A <dhafindiamle@gmail.com>
2024-03-13 21:04:31 +0700

    Add tests (#17)
    * Add test package
    
    * Add theme mode provider tests

---
+++ test/service/theme_mode_test.dart
@@ -0,0 +1,50 @@
+import 'package:dummymart/services/theme_mode.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:test/test.dart';
+
+import '../utils/testing_utils.dart';
+
+void main() {
+  group('Test theme mode provider', () {
+    test('theme should initally be ThemeMode.system', () async {
+      await openTemporaryBox<String>('settings');
+      final container = createContainer();
+
+      expect(
+        container.read(currentThemeModeProvider),
+        equals(ThemeMode.system),
+      );
+    });
+
+    test(
+        'value exposed by the provider should update when the theme is changed',
+        () async {
+      await openTemporaryBox<String>('settings');
+      final container = createContainer();
+
+      // Change the theme mode to dark.
+      container.read(currentThemeModeProvider.notifier).set(ThemeMode.dark);
+
+      expect(
+        container.read(currentThemeModeProvider),
+        equals(ThemeMode.dark),
+      );
+    });
+
+    test(
+        'value on the settings box should be updated when the theme is changed',
+        () async {
+      await openTemporaryBox<String>('settings');
+      final container = createContainer();
+
+      // Change the theme mode to light.
+      container.read(currentThemeModeProvider.notifier).set(ThemeMode.light);
+
+      expect(
+        Hive.box<String>('settings').get('themeMode'),
+        equals(ThemeMode.light.name),
+      );
+    });
+  });
+}
---
+++ test/utils/testing_utils.dart
@@ -0,0 +1,34 @@
+import 'package:hive/hive.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:test/test.dart';
+
+/// Creates a [ProviderContainer] and automatically disposes it at the end of 
+/// the test.
+ProviderContainer createContainer({
+  ProviderContainer? parent,
+  List<Override> overrides = const [],
+  List<ProviderObserver>? observers,
+}) {
+  // Create a ProviderContainer, and optionally allow specifying parameters.
+  final container = ProviderContainer(
+    parent: parent,
+    overrides: overrides,
+    observers: observers,
+  );
+
+  // When the test ends, dispose the container.
+  addTearDown(container.dispose);
+
+  return container;
+}
+
+/// Initializes Hive temporarily and opens a box. Will delete the box after 
+/// the current test.
+Future<Box<E>> openTemporaryBox<E>(String name) async {
+  Hive.init('.test-cache/temporaryPath');
+  final box = await Hive.openBox<E>(name);
+
+  addTearDown(Hive.deleteFromDisk);
+
+  return box;
+}
