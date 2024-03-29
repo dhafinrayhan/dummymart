1624971686a402546af9b89c8c202fc390670e5f
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-03-30 05:24:44 +0700

    Add more doc comments

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 extension BuildContextX on BuildContext {
+  /// A convenient way to access [ThemeData.colorScheme] of the current context.
+  ///
+  /// This also prevents confusion with a bunch of other properties of [ThemeData]
+  /// that are less commonly used.
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
+
+  /// A convenient way to access [ThemeData.textTheme] of the current context.
+  ///
+  /// This also prevents confusion with a bunch of other properties of [ThemeData]
+  /// that are less commonly used.
+  TextTheme get textTheme => Theme.of(this).textTheme;
 
   /// Shows a floating snack bar with text as its content.
   ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showTextSnackBar(
