920489018e22feaa653c3d0bb5048f9ed0a5dccf
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-03-29 23:31:54 +0700

    Remove unnecessary type alias

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-typedef ThemeDataTransformer = ThemeData Function(ThemeData);
-
 /// Creates two [ThemeData]s (light and dark variants) from a given [seedColor].
 ///
 /// This method is needed because once a [ThemeData] is generated, we cannot get
@@ -10,7 +8,7 @@ typedef ThemeDataTransformer = ThemeData Function(ThemeData);
   required Color seedColor,
   TextTheme? textTheme,
   bool? useMaterial3,
-  ThemeDataTransformer? transformer,
+  ThemeData Function(ThemeData)? transformer,
 }) {
   var lightTheme = ThemeData.from(
     colorScheme: ColorScheme.fromSeed(
