c88f573816c4a284dd2dd219c422b2b01400ad78
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-04-12 05:35:00 +0700

    Add test for PostsScreen

---
+++ test/features/posts/screens/posts_test.dart
@@ -0,0 +1,46 @@
+import 'package:dummymart/features/posts/providers/posts.dart';
+import 'package:dummymart/features/posts/screens/posts.dart';
+import 'package:dummymart/services/api/api_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../utils/testing_utils.dart';
+
+void main() {
+  group('Test PostsScreen', () {
+    testWidgets(
+      'should show title and tags of the first item',
+      (tester) async {
+        await tester.pumpMaterialWidgetScoped(
+          const PostsScreen(),
+          overrides: [
+            apiServiceProvider.overrideWith((ref) => createMockedApiClient()),
+          ],
+        );
+
+        await tester.pumpAndSettle();
+
+        final element = tester.element(find.byType(PostsScreen));
+        final container = ProviderScope.containerOf(element);
+
+        final posts = (await tester.runAsync(
+          () => container.read(postsProvider().future),
+        ))!;
+        final firstPost = posts.first;
+
+        final titleFinder = find.text(firstPost.title);
+        expect(titleFinder, findsWidgets);
+
+        final listTileFinder = find.widgetWithText(ListTile, firstPost.title);
+        for (final tag in firstPost.tags) {
+          final tagFinder = find.text(tag);
+          final tagInListTileFinder =
+              find.descendant(of: listTileFinder, matching: tagFinder);
+
+          expect(tagInListTileFinder, findsOneWidget);
+        }
+      },
+    );
+  });
+}
