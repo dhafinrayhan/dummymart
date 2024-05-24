57826ff343a7881419ae5276f887d914a2fbaa96
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-05-24 20:28:12 +0700

    Reorder fields to represent API response

--- lib/features/products/models/product.dart
+++ lib/features/products/models/product.dart
@@ -10,12 +10,12 @@ class Product with _$Product {
     required int id,
     required String title,
     required String description,
+    required String category,
     required double price,
     required double discountPercentage,
     required double rating,
     required int stock,
     String? brand,
-    required String category,
     required String thumbnail,
     required List<String> images,
   }) = _Product;
